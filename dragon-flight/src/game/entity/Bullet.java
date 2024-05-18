120b4455567486ba810a889787f13370e4929fdc
YCW <akpe12@naver.com>
2024-05-18 18:59:56 +0900

    import codes and files from main, SJW branch

--- dragon-flight/src/game/entity/Bullet.java
+++ dragon-flight/src/game/entity/Bullet.java
@@ -2,67 +2,60 @@ package game.entity;
 
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
-
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
-public class Bullet extends Entity{
+public class Bullet extends Entity {
 	private static BufferedImage bulletImg;
 	private String imgPath = "image/bullet_01.png";
 	private int speed;
 	private int damage;
 
-	
 	public Bullet(int playerX) {
 		super(playerX - 10, 512 - 100, 100, 100);
-		
+
 		loadImg(imgPath);
-		
-		this.speed = 50;
-		this.damage = 1;
-	};
-	
+
+		this.speed = 1000;
+		this.damage = 100; // current enemy's hp is 100
+	}
+
 	public void loadImg(String path) {
 		try {
-			this.bulletImg = ImageIO.read(new File(path));	
-		}
-		catch (IOException e) {
+			this.bulletImg = ImageIO.read(new File(path));
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public void move() {
-		y -= this.speed;
+
+	public void move(double dt) {
+		y -= this.speed * dt;
 	}
-	
-//	public void fireBullet(int playerX) {
-//		Bullet bullet = new Bullet(playerX);
-//		bullets.add(bullet);
-//	}
-	
+
 	public boolean isOut() {
 		return y < -100;
 	}
-	
+
 	public void setDamage(int damage) {
 		this.damage = damage;
 	}
 	
+	public int getDamage() {
+		return damage;
+	}
+
 	public double getX() {
 		return x;
 	}
-	
+
 	public double getY() {
 		return y;
 	}
-	
+
 	public void render(Graphics g) {
-		// x should be always equal to x position of player
-//		for (Bullet bullet: bullets) {
-//			g.drawImage(this.bulletImg, (int)x, (int)y, width, height, null);
-//		}
-		g.drawImage(this.bulletImg, (int)x, (int)y, width, height, null);
+		// @YCW: x should be always equal to x position of player
+		g.drawImage(this.bulletImg, (int) x, (int) y, width, height, null);
 	}
 }
